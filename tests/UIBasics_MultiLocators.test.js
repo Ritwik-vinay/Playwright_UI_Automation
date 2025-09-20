a0393c1f23e78e97e40dcd6c76413f4923afda65
ritwik vinay <ritwikvinay38@gmail.com>
2025-09-20 08:27:35 +0530

    entering calender

--- tests/UIBasics_MultiLocators.test.js
+++ tests/UIBasics_MultiLocators.test.js
@@ -10,13 +10,15 @@ test('UI All Locator End to End Practice', async ({ browser }) => {
     await page.getByText("Check me out if you Love IceCreams!").click();
     await page.getByText("Gender").selectOption("Female");
     await page.getByLabel("Employed").check();
+     await page.locator("input[name='bday']").fill("1995-12-12");
+      await expect(page.locator('input[name="bday"]')).toHaveValue('1995-12-12');
     await page.getByRole("button", { name: 'Submit' }).click();
     await page.getByRole("link", { name: 'Shop' }).click();
     await page.locator("app-card").filter({ hasText: 'Nokia Edge' }).getByRole("button").click();
     const allButtons = await page.locator("a, button").allTextContents();
     console.log(allButtons);
     await page.getByText(/Checkout/).click()
-    // await page.getByRole('link', { name: /Checkout/ }).click();
+  
 
 
 
